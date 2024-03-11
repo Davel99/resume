5da83a0e04124de4e9482abe893598dfad67d12b
davel99 <joelmp3@live.com>
2024-03-10 20:39:00 -0600

    Added pattern to background in main

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ function App() {
     <Router>
       <div className="App">
         <Header></Header>
-        <Routes>
-          <Route path='/home' Component={Home} />
-          <Route path='/' Component={Home} />
-          <Route path='/my-profile' Component={Home} />
-          <Route path='/my-experience' Component={Experience} />
-          <Route path='/my-projects' Component={Projects} />
-        </Routes>
+        <main className="bg-dark">
+          <Routes>
+            <Route path='/home' Component={Home} />
+            <Route path='/' Component={Home} />
+            <Route path='/my-profile' Component={Home} />
+            <Route path='/my-experience' Component={Experience} />
+            <Route path='/my-projects' Component={Projects} />
+          </Routes>
+        </main>
         <Footer></Footer>
       </div>
     </Router>
