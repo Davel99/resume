c196c128dfde8c97cf572c23f419ee9b76aae4b2
davel99 <joelmp3@live.com>
2024-03-10 21:17:10 -0600

    Adding content to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 const Footer = () => {
-    return(
-        <footer>
-            <h1>This is the footer</h1>
+    const curentYear = new Date().getFullYear()
+    return (
+        <footer class="bg-primary py-3">
+            <div class="d-flex py-3 text-white text-center">
+                <div class="col-sm-12 col-md-4">
+                    ¡Gracias!
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <b>JOEL DAVID GOMEZ ORTEGA</b> © <span id="yearSpan">{curentYear}</span>
+                </div>
+            </div>
         </footer>
     )
 }
