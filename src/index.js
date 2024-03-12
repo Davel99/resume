b406da3732365008d3c1e9caa0fdd53f817be3c6
davel99 <joelmp3@live.com>
2024-03-12 08:06:13 -0600

    Fixing missing component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './utility/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
