2050cff034731b5706192affe4a7e462434dbedb
davel99 <joelmp3@live.com>
2024-03-12 01:31:50 -0600

    Importing i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './config/i18n';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Home from './pages/Home';
