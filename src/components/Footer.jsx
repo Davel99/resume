ba86f73840fd692ea426c63619c18251d7aaaaa9
davel99 <joelmp3@live.com>
2024-03-11 21:02:30 -0600

    Fixed overflow in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import RepoLink from "../pages/microcomponents/RepoLink"
 const Footer = () => {
     const curentYear = new Date().getFullYear()
     return (
-        <footer class="bg-primary py-3">
-            <div class="row py-3 text-white text-center">
+        <footer class="bg-primary py-3 px-0">
+            <div class="d-flex py-3 text-white text-center">
                 <div class="col-sm-12 col-md-4 px-5">
                     <RepoLink href="https://github.com/Davel99/resume">Visita el repositorio de esta web</RepoLink>
                 </div>
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-8 px-0">
                     <b>JOEL DAVID GOMEZ ORTEGA</b> © <span id="yearSpan">{curentYear}</span>
                 </div>
             </div>
