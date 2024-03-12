dc794cd444d525c54cc3826afef44b11b39cc678
davel99 <joelmp3@live.com>
2024-03-12 00:24:23 -0600

    Improved styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Home from './pages/Home';
-import i18n from './config/i18n';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import ScrollToTop from './utility/ScrollToTop';
