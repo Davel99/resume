7b159aa87980b2df3531129c25437aecab81deee
davel99 <joelmp3@live.com>
2024-03-12 00:19:21 -0600

    Improved visualization on mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const curentYear = new Date().getFullYear();
     return (
         <footer class="bg-primary py-3 px-0">
-            <div class="d-flex py-3 text-white text-center">
+            <div class="row px-0 mx-0 py-3 text-white text-center">
                 <div class="col-sm-12 col-md-4 px-5">
                     <RepoLink href="https://github.com/Davel99/resume">
                         {t('gral.visit_repo')}
