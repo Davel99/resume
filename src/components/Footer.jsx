19b5fb54fd8941f5e397d1334b66136bdd8a39b1
davel99 <joelmp3@live.com>
2024-03-11 22:13:48 -0600

    Added messages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 import RepoLink from "../pages/microcomponents/RepoLink"
+import { useTranslation } from "react-i18next"
 
 const Footer = () => {
-    const curentYear = new Date().getFullYear()
+    const { t } = useTranslation();
+    const curentYear = new Date().getFullYear();
     return (
         <footer class="bg-primary py-3 px-0">
             <div class="d-flex py-3 text-white text-center">
                 <div class="col-sm-12 col-md-4 px-5">
-                    <RepoLink href="https://github.com/Davel99/resume">Visita el repositorio de esta web</RepoLink>
+                    <RepoLink href="https://github.com/Davel99/resume">
+                        {t('gral.visit_repo')}
+                    </RepoLink>
                 </div>
                 <div class="col-sm-12 col-md-8 px-0">
                     <b>JOEL DAVID GOMEZ ORTEGA</b> © <span id="yearSpan">{curentYear}</span>
